3cc3cfeff3c0c6a0722e7eef6133b11d1eaabb07
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-02 11:00:23 -0300

    feat: add User-Agent header to request

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests, re
 
 url = 'https://econodata.com.br/consulta-empresa?filtro=60499310000124'
 
-response = requests.get(url)
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'})
 
 text = response.text
 
