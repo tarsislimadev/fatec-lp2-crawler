f292bf651b3c4e71292a4558eeb430af530ec0d4
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-04-02 10:47:23 -0300

    feat: implement web crawler to extract CNPJ data and add requirements file

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Fatec Crawler LP2')
+import requests, re
+
+url = 'https://econodata.com.br/consulta-empresa?filtro=60499310000124'
+
+response = requests.get(url)
+
+text = response.text
+
+with open('empresas.html', 'w') as f: f.write(text)
+
+cnpjs = re.findall(r'\d\d\.\d\d\d\.\d\d\d\/\d\d\d\d\-\d\d', text)
+
+print(cnpjs)
